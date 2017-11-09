ef0366241ca0da6db4332761c8308468ee3f781c
Novak-Oksana <oksana.babaeva@gmail.com>
2017-11-10 00:51:01 +0200

    Changed UnitTests

--- CalcTest/UnitTest1.cs
+++ CalcTest/UnitTest1.cs
@@ -7,17 +7,45 @@ namespace CalcTest
     [TestClass]
     public class UnitTest1
     {
-        [DataTestMethod]
-        [DataRow(1, 2, "+", 3)]
-        [DataRow(1, 2, "*", 2)]
-        [DataRow(1, 2, "-", -1)]
-        [DataRow(2, 2, "/", 1)]
+        //[DataTestMethod]
+        //[DataRow(1, 2, "+", 3)]
+        //[DataRow(1, 2, "*", 2)]
+        //[DataRow(1, 2, "-", -1)]
+        //[DataRow(2, 2, "/", 1)]
+
+        //[TestMethod()]
+        //public void funct_calcTest(int a, int b, string op, int exp)
+        //{
+        //    int res = HttpServer.Calc(a, b, op);
+        //    Assert.AreEqual(exp, res); ;
+        //}
 
         [TestMethod()]
-        public void funct_calcTest(int a, int b, string op, int exp)
+        public void calcTest1()
         {
-            int res = HttpServer.Calc(a, b, op);
-            Assert.AreEqual(exp, res); ;
+            int res = HttpServer.Calc(1, 2, "+");
+            Assert.AreEqual(3, res); ;
+        }
+
+        [TestMethod()]
+        public void calcTest2()
+        {
+            int res = HttpServer.Calc(1, 2, "*");
+            Assert.AreEqual(2, res); ;
+        }
+
+        [TestMethod()]
+        public void calcTest3()
+        {
+            int res = HttpServer.Calc(1, 2, "-");
+            Assert.AreEqual(-1, res); ;
+        }
+
+        [TestMethod()]
+        public void calcTest4()
+        {
+            int res = HttpServer.Calc(2, 2, "/");
+            Assert.AreEqual(1, res); ;
         }
     }
 }
