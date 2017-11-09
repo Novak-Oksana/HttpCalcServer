858e22cbdea2300fb1eb950e816bfcd06ff1e300
Novak-Oksana <oksana.babaeva@gmail.com>
2017-11-09 18:30:58 +0200

    Add project files.

---
+++ HttpCalcServer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpCalcServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              HttpServer server = new HttpServer("http://localhost:8888/");
+           // HttpServer server = new HttpServer("http://192.168.0.118:9050/");
+            server.Start();
+        }
+    }
+}
